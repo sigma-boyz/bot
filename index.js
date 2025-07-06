4bd0322d9196630bf12b403b29d3622690bad7e1
sigma-boyz <sigma-sigma-boyz@outlook.com>
2025-07-06 20:54:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(8000, () => console.log('Web server started on port 8000'));
 // === SERVER CONFIGURATION (Only IP/Port here) ===
 const servers = [
   { ip: 'Sigma-Sigma-boyz.aternos.me', port: 37216 },
-  { ip: 'manhunt-6juW.aternos.me', port: 56527 }
+  { ip: 'MColab.aternos.me', port: 38054 }
 ];
 
 // === State Tracking ===
